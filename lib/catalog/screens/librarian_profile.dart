4073947027cdbadc8f62c1ec2c2be6ad83772a87
evelynphs <evelyn.p.h.silalahi@gmail.com>
2023-12-09 13:37:47 +0700

    show librarian's username

--- lib/catalog/screens/librarian_profile.dart
+++ lib/catalog/screens/librarian_profile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:letterbookd/main.dart';
 
 class LibrarianProfile extends StatefulWidget {
   const LibrarianProfile({super.key});
@@ -9,24 +12,44 @@ class LibrarianProfile extends StatefulWidget {
 }
 
 class _LibrarianProfileState extends State<LibrarianProfile> {
-  // TODO: show librarian's username
+
+  Future<String> _getUsername(
+    CookieRequest request,
+  ) async {
+    final response = await request.get('${AppData().url}/catalog/get-username/');
+
+    return response['username'];
+  }
+
   @override
   Widget build(BuildContext context) {
+    CookieRequest request = context.watch<CookieRequest>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Book Detail'),
       ),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 20),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 20.0),
-            Text(
-              "Username: []",
-              style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold,),
+            const SizedBox(height: 20.0),
+            FutureBuilder<String>(
+              future: _getUsername(request),
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) {
+                  return const Center(child: CircularProgressIndicator());
+                } 
+                else {
+                  return Text(
+                    "Username: ${snapshot.data}",
+                    style: const TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold,),
+                  );
+                }
+              },
             ),
-            SizedBox(height: 7.0),
+            const SizedBox(height: 7.0),
           ],
         ),
       ),
