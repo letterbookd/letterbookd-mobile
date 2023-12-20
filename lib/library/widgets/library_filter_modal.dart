d5e88630444f66eff33ca625be2d892d1e07b0fc
mma <111668501+mmalkindi@users.noreply.github.com>
2023-12-20 19:34:51 +0700

    reordered filter modal

--- lib/library/widgets/library_filter_modal.dart
+++ lib/library/widgets/library_filter_modal.dart
@@ -11,10 +11,10 @@ class _LibraryFilterModalState extends State<LibraryFilterModal>
     with SingleTickerProviderStateMixin {
   static const List<Tab> modalTabs = <Tab>[
     Tab(
-      text: "Sort",
+      text: "Filter",
     ),
     Tab(
-      text: "Filter",
+      text: "Sort",
     ),
     Tab(
       text: "Display",
@@ -65,128 +65,10 @@ class _LibraryFilterModalState extends State<LibraryFilterModal>
                         Expanded(
                           child: TabBarView(
                             controller: _controller,
-                            children: <Widget>[
-                              SingleChildScrollView(
-                                child: Column(
-                                  children: <Widget>[
-                                    ListTile(
-                                      leading: const Icon(Icons.sort_by_alpha),
-                                      title: const Text('Ttile'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'title');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading:
-                                          const Icon(Icons.schedule_rounded),
-                                      title: const Text('Recently Added'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'recentlyAdded');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading: const Icon(Icons.label_outline),
-                                      title: const Text('Tracking Status'),
-                                      onTap: () {
-                                        Navigator.pop(
-                                            context, 'trackingStatus');
-                                      },
-                                    ),
-                                    const Divider(),
-                                    ListTile(
-                                      leading: const Icon(Icons.arrow_upward),
-                                      title: const Text('Sort Ascending'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'ascending');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading: const Icon(Icons.arrow_downward),
-                                      title: const Text('Sort Descending'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'descending');
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              SingleChildScrollView(
-                                child: Column(
-                                  children: <Widget>[
-                                    ListTile(
-                                      leading: const Icon(
-                                          Icons.align_horizontal_left_sharp),
-                                      title: const Text('All'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'all');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading:
-                                          const Icon(Icons.favorite_outline),
-                                      title: const Text('Favorites'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'favorites');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading: const Icon(Icons.done_all),
-                                      title: const Text('Finished Reading'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'finished');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading: const Icon(Icons.done),
-                                      title: const Text('Currently Reading'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'reading');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading: const Icon(Icons.pause_outlined),
-                                      title: const Text('On Hold'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'onHold');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading: const Icon(Icons.calendar_month),
-                                      title: const Text('Planning to Read'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'planned');
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading: const Icon(Icons.close),
-                                      title: const Text('Dropped'),
-                                      onTap: () {
-                                        Navigator.pop(context, 'dropped');
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Column(
-                                children: <Widget>[
-                                  ListTile(
-                                    leading:
-                                        const Icon(Icons.grid_view_outlined),
-                                    title: const Text('Grid View'),
-                                    onTap: () {
-                                      Navigator.pop(context, 'grid');
-                                    },
-                                  ),
-                                  ListTile(
-                                    leading:
-                                        const Icon(Icons.view_list_outlined),
-                                    title: const Text('List View'),
-                                    onTap: () {
-                                      Navigator.pop(context, 'list');
-                                    },
-                                  ),
-                                ],
-                              ),
+                            children: const <Widget>[
+                              FilterSelections(),
+                              SortSelections(),
+                              DisplaySelctions(),
                             ],
                           ),
                         )
@@ -194,6 +76,147 @@ class _LibraryFilterModalState extends State<LibraryFilterModal>
                 ),
               ));
         }),
+      ),
+    );
+  }
+}
+
+class SortSelections extends StatelessWidget {
+  const SortSelections({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.sort_by_alpha),
+            title: const Text('Ttile'),
+            onTap: () {
+              Navigator.pop(context, 'title');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.schedule_rounded),
+            title: const Text('Recently Added'),
+            onTap: () {
+              Navigator.pop(context, 'recentlyAdded');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.label_outline),
+            title: const Text('Tracking Status'),
+            onTap: () {
+              Navigator.pop(context, 'trackingStatus');
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.arrow_upward),
+            title: const Text('Sort Ascending'),
+            onTap: () {
+              Navigator.pop(context, 'ascending');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.arrow_downward),
+            title: const Text('Sort Descending'),
+            onTap: () {
+              Navigator.pop(context, 'descending');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FilterSelections extends StatelessWidget {
+  const FilterSelections({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.align_horizontal_left_sharp),
+            title: const Text('All'),
+            onTap: () {
+              Navigator.pop(context, 'all');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.favorite_outline),
+            title: const Text('Favorites'),
+            onTap: () {
+              Navigator.pop(context, 'favorites');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.done_all),
+            title: const Text('Finished Reading'),
+            onTap: () {
+              Navigator.pop(context, 'finished');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.done),
+            title: const Text('Currently Reading'),
+            onTap: () {
+              Navigator.pop(context, 'reading');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.pause_outlined),
+            title: const Text('On Hold'),
+            onTap: () {
+              Navigator.pop(context, 'onHold');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.calendar_month),
+            title: const Text('Planning to Read'),
+            onTap: () {
+              Navigator.pop(context, 'planned');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.close),
+            title: const Text('Dropped'),
+            onTap: () {
+              Navigator.pop(context, 'dropped');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DisplaySelctions extends StatelessWidget {
+  const DisplaySelctions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.grid_view_outlined),
+            title: const Text('Grid View'),
+            onTap: () {
+              Navigator.pop(context, 'grid');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.view_list_outlined),
+            title: const Text('List View'),
+            onTap: () {
+              Navigator.pop(context, 'list');
+            },
+          ),
+        ],
       ),
     );
   }
